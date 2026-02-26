953bdfedcd85ce0eee4693a6a88f14e833da9fbc
RaulBertullo <rabeal@alumnatflorida.es>
2026-02-26 14:13:34 +0100

    ProyectoCreado

---
+++ Partycle3D/Assets/PartículaCarpeta/scripts/Particle.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public float velocidadInicial;
+    public float anguloIncial;
+    public float tiempoVida;
+    public float tiempoActivo;
+    public float gravedad;
+
+    public GameObject sprite;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Partycle3D/Assets/PartículaCarpeta/scripts/ParticlesController.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class ParticlesController : MonoBehaviour
+{
+    public int num_particles;
+    public float initial_velocity;
+    public float initial_angle;
+    public float lifeTime;
+    public float gravity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateParticleExplosion()
+    {
+        
+    }
+}
