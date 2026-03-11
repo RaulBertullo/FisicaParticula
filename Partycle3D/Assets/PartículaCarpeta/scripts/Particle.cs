3758336ef8e11a11a752ff261a3349c9a765ccc6
RaulBertullo <rabeal@alumnatflorida.es>
2026-03-11 19:59:09 +0100

    Creación de partículas en 3D
    Partículas hechas en 3D

--- Partycle3D/Assets/PartículaCarpeta/scripts/Particle.cs
+++ Partycle3D/Assets/PartículaCarpeta/scripts/Particle.cs
@@ -4,10 +4,11 @@ public class Particle : MonoBehaviour
 {
     public float velocidadInicial;
     public float anguloIncial;
+    public float anguloInicialZ;
     public float tiempoVida;
     public float tiempoActivo;
     public float gravedad;
-
+    
     public GameObject sprite;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +19,18 @@ public class Particle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        tiempoActivo += Time.deltaTime;
+        float AnguloRadianes = Mathf.Rad2Deg * anguloIncial;
+        float AnguloRadianesZ = Mathf.Rad2Deg * anguloInicialZ;
+        float x = velocidadInicial * Mathf.Cos(AnguloRadianes) * tiempoActivo;
+        float y = velocidadInicial * Mathf.Sin(AnguloRadianes) * tiempoActivo - 0.5f * gravedad * tiempoActivo * tiempoActivo;
+        float z = velocidadInicial * Mathf.Cos(AnguloRadianesZ) * tiempoActivo;
+
+        transform.position = new Vector3(x, y, z);
         
+        if (tiempoActivo > tiempoVida)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Partycle3D/Assets/PartículaCarpeta/scripts/ParticlesController.cs
+++ Partycle3D/Assets/PartículaCarpeta/scripts/ParticlesController.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using System.Collections.Generic;
 
 public class ParticlesController : MonoBehaviour
@@ -9,9 +10,11 @@ public class ParticlesController : MonoBehaviour
     public int num_particles;
     public float initial_velocity;
     public float initial_angle;
+    public float initial_anglez;
     public float lifeTime;
     public float gravity;
-
+    private float tiempoSpawn;
+    public float tiempoIntervalo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,11 +24,27 @@ public class ParticlesController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        tiempoSpawn += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CreateParticleExplotion();
+            tiempoSpawn = 0;
+        }
     }
 
-    private void CreateParticleExplosion()
+    private void CreateParticleExplotion()
     {
+        for(int i = 0; i < num_particles; i++)
+        {
+            GameObject parti = Instantiate(Particle, transform.position, Quaternion.identity);
+            Particle Script = parti.GetComponent<Particle>();
+
+            Script.velocidadInicial = initial_velocity * Random.Range(1, 3);
+            Script.anguloIncial = initial_angle * Random.Range(1, 360);
+            Script.anguloInicialZ = initial_anglez * Random.Range(1, 360);
+            Script.tiempoVida = lifeTime;
+            Script.gravedad = gravity * Random.Range(1, 3);
+        }
         
     }
 }
